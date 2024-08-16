1ef0c913719fc06fa6eb31d5faae25c3b9b304c3
R PRAVEEN KUMAR <praveenrameshr2@gmail.com>
2024-08-16 13:48:02 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://task-management-app-back-end-side.onrender.com'
+    baseURL: 'https://task-management-app-back-end-side-1.onrender.com'
 },
 {
    Credentials: 'include',
